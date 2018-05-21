e9412d614bffa6fcc23ef31ca5a880ea55f01131
Jason Morganson <jason@morganson.me>
2018-05-21 01:37:29 +0000

    Remove avatar

--- source/cli.js
+++ source/cli.js
@@ -14,11 +14,4 @@ meow(`
 	  $ jasonmorganson
 `);
 
-const fallback = async () => {
-	const image = await terminalImage.file(path.join(__dirname, 'avatar-fallback.png'));
-	console.log(image);
-};
-
-termImg(path.join(__dirname, 'avatar.png'), {fallback});
-
 render(h(ui));
